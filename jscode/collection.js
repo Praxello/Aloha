96671acd496031a00b6c3252f1b4d4dd17d78254
vbpawar <vikaspawar3110@gmail.com>
2020-03-05 15:29:39 +0530

    add change

--- jscode/collection.js
+++ jscode/collection.js
@@ -53,6 +53,26 @@ const getCollection = (fromDate, uptoDate, branch) => {
             $('#collectionD').html(tblData);
             $('#amtO').html(amtO.toLocaleString('en-IN', { style: 'currency', currency: 'INR' }));
             $('#collectionT').dataTable({
+                initComplete: function() {
+                    this.api().columns().every(function() {
+                        var column = this;
+                        var select = $('<select><option value=""></option></select>')
+                            .appendTo($(column.footer()).empty())
+                            .on('change', function() {
+                                var val = $.fn.dataTable.util.escapeRegex(
+                                    $(this).val()
+                                );
+
+                                column
+                                    .search(val ? '^' + val + '$' : '', true, false)
+                                    .draw();
+                            });
+
+                        column.data().unique().sort().each(function(d, j) {
+                            select.append('<option value="' + d + '">' + d + '</option>')
+                        });
+                    });
+                },
                 searching: true,
                 retrieve: true,
                 bPaginate: $('tbody tr').length > 10,
