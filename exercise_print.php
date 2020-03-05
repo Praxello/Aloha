f2150da295226bf499b2395d6439113fa0eef9a5
vbpawar <vikaspawar3110@gmail.com>
2020-03-05 19:27:39 +0530

    add an change

--- exercise_print.php
+++ exercise_print.php
@@ -64,7 +64,7 @@ function exexcise_details($patientId,$visitDate){
     $result=mysqli_query($conn,$sql);
     if (mysqli_num_rows($result) > 0) {
         $output .= '<div class="col-xs-12">
-        <table class="table ">
+        <table class="table">
             <thead>
                 <tr>
                     <th style="text-align:left" class="border-1 text-uppercase small font-weight-bold ">Photo</th>
@@ -77,8 +77,7 @@ function exexcise_details($patientId,$visitDate){
             <tbody>';
         while ($row = mysqli_fetch_array($result)) {
        
-            $output .= ' <tr>
-
+            $output .= '<tr>
         <td style="width="100%">  <img class="img-fluid" src="upload/exercise/'.$row['Id'].'.jpg" width="500% " height="600%"></td>
         <td style="text-align:left">'.$row['title'].' </td>
         <td style="text-align:left">'.$row['details'].' </td>
@@ -90,14 +89,13 @@ function exexcise_details($patientId,$visitDate){
     return $output;     
 
 }
-$html = '
-<html>
-
+$html = '<link rel="stylesheet" href="dompdf/style.css">
 <head>
-<link rel="stylesheet" href="dompdf/style.css">
-    
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <style>
+  *{ font-family: DejaVu Sans !important;}
+</style>
 </head>
-
 <body>
 
 <div class="row">
@@ -118,17 +116,12 @@ $html = '
 
 <div class="row">
     <div class="col-md-12">
-                           
-                       '.exexcise_details($patientId,$visitDate).'
-
-        
+    '.exexcise_details($patientId,$visitDate).' 
     </div>
 </div>
-
 </body>
-
 </html>';
-$dompdf->setPaper('A4', 'portrait');
+
 $dompdf->loadHtml($html);
 
 /* Render the HTML as PDF */
@@ -136,7 +129,6 @@ $dompdf->render();
 
 /* Output the generated PDF to Browser */
 // $dompdf->stream();
-$dompdf->stream("dompdf_out.pdf", array("Attachment" => false));
-
+$dompdf->stream("exercise.pdf", array("Attachment" => false));
 exit(0);
 ?>
